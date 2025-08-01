1e32dc2510ab339f7de729ca50070399a7b87a2f
Eduard Vilensky <embox53@gmail.com>
2025-08-01 22:08:23 +0300

    auth screen background/logo

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.jsm.foodordering",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "688d0cb30013b845ab07",
+  userCollectionId: "688d0cea003e3ade06ad",
 };
